91b55e6645bda533dbdb00cf5d8382fc0d67dc6e
Brian Dickson <brianlawrencedickson@gmail.com>
2023-07-18 11:50:39 +0100

    test2

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -2938,16 +2938,15 @@ var scroll = new _locomotiveScroll.default({
     smooth: true
   }
 });
-
-// setTimeout(() => {  
-//     locoScroll.destroy();
-// }, 0);
-// setTimeout(() => {  
-//     locoScroll.init();
-// }, 50);
-// setTimeout(() => {  
-//     locoScroll.update();
-// }, 1000);
+setTimeout(function () {
+  locoScroll.destroy();
+}, 0);
+setTimeout(function () {
+  locoScroll.init();
+}, 50);
+setTimeout(function () {
+  locoScroll.update();
+}, 1000);
 },{"locomotive-scroll":"../node_modules/locomotive-scroll/dist/locomotive-scroll.esm.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -2973,7 +2972,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64057" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64221" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
